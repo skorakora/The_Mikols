3a7ed66da2a5f253728b4df7000f30e358df1795
Daniel Skorski <skorski.6a.20.2012@wp.pl>
2024-08-05 10:22:39 +0200

    WIP, main script

--- Editor.gd
+++ Editor.gd
@@ -1,6 +1,8 @@
 extends Node3D
 
-var SCN_Path: String
+var Simulator_Resource_Path: String = "/home/baniel/Desktop/Maszyna/"
+
+var SCN_Name: String
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,4 +11,13 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	
+	pass
+
+func SCN_Deserialize(): #Deserialize
+	print("Deserializing scenery:" + SCN_Name)
+	
+	
+
+func SCN_Serialize:
 	pass
